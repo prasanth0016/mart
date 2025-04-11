08a489e4ee111aa1dceec5dacd7c609fc1ddd453
prasanth0016 <gprasanth0016@gmail.com>
2025-04-11 14:44:23 +0530

    slider updated

--- shoppingMart/src/Header.jsx
+++ shoppingMart/src/Header.jsx
@@ -5,15 +5,15 @@ import { NavLink, Link } from 'react-router-dom'
 export default function Header() {
   return (
     <>
-      <header className='shadow'>
+      <header className='shadow sticky-top bg-white'>
         <div className='d-flex justify-content-between align-items-center pad'>
           <div>
             <Link to='/' className='text-decoration-none text-dark fontSize'><i className="fa-solid fa-shop"></i> s-Mart</Link>
           </div>
-          <div className='d-flex gap-3 fw-bold'>
+          <div className='d-flex gap-3 fw-bold fontForMobile'>
             <NavLink to='/home' className={(e) => e?.isActive ? "activelink text-decoration-none" : "text-dark text-decoration-none"}>Home</NavLink>
             <NavLink to='/shop' className={(e) => e?.isActive ? "activelink text-decoration-none" : "text-dark text-decoration-none"}>Shop</NavLink>
-            <NavLink to='/cart' className={(e) => e?.isActive ? "activelink text-decoration-none" : "text-dark text-decoration-none"}>Cart <i className="fa-solid fa-cart-flatbed-suitcase"></i></NavLink><div className='cartnum'>0</div>
+            <NavLink to='/cart' className={(e) => e?.isActive ? "activelink text-decoration-none" : "text-dark text-decoration-none"}>Cart<i className="fa-solid fa-cart-flatbed-suitcase"></i></NavLink><div className='cartnum'>0</div>
           </div>
         </div>
       </header>
--- shoppingMart/src/Home.jsx
+++ shoppingMart/src/Home.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { SliderData } from './data/products'
 
 export default function Home() {
+    const [i, setI] = useState(0)
+    useEffect(() => {
+        const slide = setInterval(() => {
+            setI((prevI) => (prevI + 1) % SliderData.length)
+        }, 2000)
+        return () => clearInterval(slide);
+    }, [])
     return (
-        <div>Home</div>
+        <>
+            <div className='w-100 border mt-5 slidemaindiv d-flex justify-content-center align-items-center'>
+                <div className='d-flex justify-content-center align-items-center w-75 h-100 border'>
+                    <div className=''>
+                        <h5 className='fontForMobile'>{SliderData[i].title}</h5>
+                        <p className='fontForMobile'>{SliderData[i].desc}</p>
+                    </div>
+                    <img className='h-100 border w-75' src={SliderData[i].cover} alt={SliderData[i].title} />
+                </div>
+            </div>
+        </>
     )
 }
